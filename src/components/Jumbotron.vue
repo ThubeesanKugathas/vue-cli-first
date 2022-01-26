fd124a63adfdb5e28342f65b198780c5240c0480
ThubeesanKugathas <Thubeesan@outlook.com>
2022-01-26 16:37:07 +0100

    jumbotron component (bonus)

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="jumbotron">
+        <h1>Questa è una jumbotron!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .jumbotron {
+        height: 500px;
+        background-image: url(../assets/banner.jpg);
+        background-position: center; 
+        background-repeat: no-repeat; 
+        background-size: cover;
+    }
+
+    .jumbotron h1 {
+        line-height: 500px;
+    }
+</style>
